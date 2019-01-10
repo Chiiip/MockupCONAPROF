a04c77f8be56c7d08bd6ba0873b1ce3fd8c8111b
Pedro Augusto <padncoreia@tjba.jus.br>
2019-01-09 23:43:05 -0300

    showing temperature on top of page

--- js/init.js
+++ js/init.js
@@ -1,12 +1,14 @@
 
 function renderizarTemperatura(data) {
-  $.get(`http://openweathermap.org/img/w/${data.weather[0].icon}.png`, function( data ) {
-    console.log(data);
-  });
+  document.querySelector("#temperatura").innerHTML = 
+  `<div class="inner-temperatura"> <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"></img>
+  <p>${data.main.temp}ºC</p>
+  </div>
+  <p class="descricao-temperatura">${data.weather[0].description}</p>`;
 }
 
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
@@ -14,12 +16,12 @@ function renderizarTemperatura(data) {
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('.modal').modal();
   $('.modal-trigger').modal();
 
-  $.get( "https://api.openweathermap.org/data/2.5/weather?q=Salvador,br&appid=933caccadb7cb8c0a6ebbd47718c120e&lang=pt&units=metric", function( data ) {
-    console.log(data);  
+  $.get("https://api.openweathermap.org/data/2.5/weather?q=Salvador,br&appid=933caccadb7cb8c0a6ebbd47718c120e&lang=pt&units=metric", function (data) {
+    console.log(data);
     renderizarTemperatura(data);
   });
 
@@ -43,62 +45,62 @@ $(document).ready(function() {
     return retorno;
   }
 
-  $('.scroll-animations .animated').each(function() {
+  $('.scroll-animations .animated').each(function () {
     if (isScrolledIntoView(this) === true) {
       $(this).addClass('fadeIn');
     }
   });
 
-  $('.scroll-animations-left .animated').each(function() {
+  $('.scroll-animations-left .animated').each(function () {
     if (isScrolledIntoView2(this) === true) {
       $(this).addClass('fadeInLeft slow');
     }
   });
 
-  $('.scroll-animations-left .animatedRight').each(function() {
+  $('.scroll-animations-left .animatedRight').each(function () {
     if (isScrolledIntoView2(this) === true) {
       $(this).addClass('fadeInRight slow');
     }
   });
 
-  $('.count-up').each(function() {
+  $('.count-up').each(function () {
     if (isScrolledIntoView(this) === true) {
-    const valor = $(this).attr("value");
-    const valorNumerico = Number.parseInt(valor);
-    const id = $(this).attr("id");
-    const demo = new CountUp(id, 0, valorNumerico);
-    demo.start();
-    $(this).removeClass('count-up');
-    }
-  });
-
-  $(window).scroll(function() {
-    $('.scroll-animations .animated').each(function() {
-      if (isScrolledIntoView(this) === true) {
-        $(this).addClass('fadeIn');
-      }
-    });
-
-    $('.scroll-animations-left .animated').each(function() {
-      if (isScrolledIntoView2(this) === true) {
-        $(this).addClass('fadeInLeft slow');
-      }
-    });
-
-    $('.scroll-animations-left .animatedRight').each(function() {
-      if (isScrolledIntoView2(this) === true) {
-        $(this).addClass('fadeInRight slow');
-      }
-    });
-
-    $('.count-up').each(function() {
-      if (isScrolledIntoView(this) === true) {
       const valor = $(this).attr("value");
       const valorNumerico = Number.parseInt(valor);
       const id = $(this).attr("id");
       const demo = new CountUp(id, 0, valorNumerico);
       demo.start();
       $(this).removeClass('count-up');
+    }
+  });
+
+  $(window).scroll(function () {
+    $('.scroll-animations .animated').each(function () {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeIn');
+      }
+    });
+
+    $('.scroll-animations-left .animated').each(function () {
+      if (isScrolledIntoView2(this) === true) {
+        $(this).addClass('fadeInLeft slow');
+      }
+    });
+
+    $('.scroll-animations-left .animatedRight').each(function () {
+      if (isScrolledIntoView2(this) === true) {
+        $(this).addClass('fadeInRight slow');
+      }
+    });
+
+    $('.count-up').each(function () {
+      if (isScrolledIntoView(this) === true) {
+        const valor = $(this).attr("value");
+        const valorNumerico = Number.parseInt(valor);
+        const id = $(this).attr("id");
+        const demo = new CountUp(id, 0, valorNumerico);
+        demo.start();
+        $(this).removeClass('count-up');
       }
     });
 
@@ -107,9 +109,8 @@ $(document).ready(function() {
 
 
 function initMap() {
-  var salvador = {lat: -12.9704, lng: -38.5124};
+  var salvador = { lat: -12.9704, lng: -38.5124 };
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: salvador, gestureHandling: 'greedy'});
-  var marker = new google.maps.Marker({position: salvador, map: map});
+    document.getElementById('map'), { zoom: 4, center: salvador, gestureHandling: 'greedy' });
+  var marker = new google.maps.Marker({ position: salvador, map: map });
 }
-
